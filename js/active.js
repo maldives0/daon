5a96ae838bbecb567c1c42086d37a8e4b40de902
maldives0 <maliethy@gmail.com>
2020-08-26 22:55:57 +0900

    update scrolling use jequery

--- js/active.js
+++ js/active.js
@@ -1,21 +1,29 @@
+
+//scroll
 window.addEventListener('DOMContentLoaded', function () {
-    var main = document.querySelector('main');
-    var mDiv = document.querySelectorAll('main div');
-
+    // var main = document.querySelector('main');
+    // var mDiv = document.querySelectorAll('main .main');
+    //mouse wheel
+   
+   
 });
-
+//nav toggle
 window.addEventListener('DOMContentLoaded', function () {
     var mTri = document.querySelector('.menu-trigger');
+
     var nav = document.querySelector('.header nav');
+  
     mTri.addEventListener('click',
     function() {
         mTri.classList.toggle('active');
         nav.classList.toggle('active');
-       
+
     });
-    
+   
+   
+
 });
-    
+
 
 
 
